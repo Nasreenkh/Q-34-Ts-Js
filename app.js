de963be59329e21cd2452c9c06dc9068ddcb1584
Nasreen khan <160172100+Nasreenkh@users.noreply.github.com>
2024-05-16 01:33:11 +0500

    This is 45 Question Assignment Q-34.
    This is 45 Question Assignment Q-34.

---
+++ app.js
@@ -0,0 +1,8 @@
+// Array of favorite pizza names
+var pizzas = ['Pepperoni', 'Margherita', 'BBQ Chicken'];
+// Loop through the array to print each pizza name and a corresponding statement
+for (var i = 0; i < pizzas.length; i++) {
+    console.log("I like ".concat(pizzas[i], " pizza."));
+}
+// Statement about how much you like pizza
+console.log("Pizza is one of my favorite foods, I really love it!");
---
+++ app.ts
@@ -0,0 +1,10 @@
+// Array of favorite pizza names
+const pizzas: string[] = ['Pepperoni', 'Margherita', 'BBQ Chicken'];
+
+// Loop through the array to print each pizza name and a corresponding statement
+for (let i = 0; i < pizzas.length; i++) {
+    console.log(`I like ${pizzas[i]} pizza.`);
+}
+
+// Statement about how much you like pizza
+console.log("Pizza is one of my favorite foods, I really love it!");
